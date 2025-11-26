bea487fc834b2b7d364a56245793bc38a0831abf
Abhi-karthi <karthiicode@icloud.com>
2025-11-26 11:31:14 -0600

    added javascript file

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+let aboutUsButton = document.querySelector('.about-us-button');
+let getSuppliesButton = document.querySelector('.get-supplies-button');
+let donateSuppliesButton = document.querySelector('.donate-supplies-button');
+let supportButton = document.querySelector('.support-button');
 
-document.querySelector('.message').textContent = 'Correct Number!';
+let aboutUsSection = document.querySelector('.about-us');
+let getSuppliesSection = document.querySelector('.Get-supplies');
+let donateSuppliesSection = document.querySelector('.donate-supplies');
+let supportSection = document.querySelector('.Support');
+
+aboutUsButton.addEventListener('click', function() {
+    aboutUsSection.classList.remove('hidden');
+    getSuppliesSection.classList.add('hidden');
+    donateSuppliesSection.classList.add('hidden');
+    supportSection.classList.add('hidden');
+});
+
+getSuppliesButton.addEventListener('click', function() {
+    aboutUsSection.classList.add('hidden');
+    getSuppliesSection.classList.remove('hidden');
+    donateSuppliesSection.classList.add('hidden');
+    supportSection.classList.add('hidden');
+});
+
+donateSuppliesButton.addEventListener('click', function() {
+    aboutUsSection.classList.add('hidden');
+    getSuppliesSection.classList.add('hidden');
+    donateSuppliesSection.classList.remove('hidden');
+    supportSection.classList.add('hidden');
+});
+
+supportButton.addEventListener('click', function() {
+    aboutUsSection.classList.add('hidden');
+    getSuppliesSection.classList.add('hidden');
+    donateSuppliesSection.classList.add('hidden');
+    supportSection.classList.remove('hidden');
+});
